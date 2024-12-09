4d3b245915bfe4e24c5a570e37dcccc39cca81b1
Nancy <nancy.cima.bertoni@gmail.com>
2024-12-09 12:02:38 -0300

    añadir resolucion de problema 1

--- Problema1.py
+++ Problema1.py
@@ -1,7 +1,17 @@
-# Importamos librerias necesarias
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
+"""
+--------------------------------------------------------------------------------
+                    DETECCION AUTOMATICA DE TIRADAS DE DADOS 
+--------------------------------------------------------------------------------
+
+Descripción:
+Este script utiliza procesamiento de imágenes para detectar tiradas de dados 
+en videos. Incluye funciones para cargar frames, procesarlos y detectar los 
+dados con sus respectivos valores.
+"""
 
 def imshow(img, new_fig=True, title=None, color_img=False, blocking=True, colorbar=False, ticks=False):
     """Definimos funcion para simplificar codigo de visualizaciones"""
@@ -19,95 +29,242 @@ def imshow(img, new_fig=True, title=None, color_img=False, blocking=True, colorb
     if new_fig:        
         plt.show(block=blocking)
 
-def detectar_movimiento(video_path, umbral_movimiento=5000, escala=0.5):
-    """Funcion para detectar el momento en que los dados 
-    de un video dejan de moverse en un video.
-    """
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        raise IOError("No se pudo abrir el video.")
+def subplot3(img1, img2, img3, share = False, t1 = "", t2 = "", t3 = ""):
+    """Recibe 3 imagenes y 3 titulos y las muestra en forma de subplot de 3x3"""
+    plt.figure()
+    if share:
+        ax = plt.subplot(131); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(132, sharex=ax, sharey=ax); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(133, sharex=ax, sharey=ax); imshow(img3, new_fig=False, title=t3)
+        plt.show(block=True)
+    else: 
+        plt.subplot(131); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(132); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(133); imshow(img3, new_fig=False, title=t3)
+        plt.show(block=True)
 
-    ret, frame_actual = cap.read()
-    if not ret:
-        raise IOError("No se pudo leer el primer frame.")
-    
-    frame_actual = cv2.resize(frame_actual, None, fx=escala, fy=escala)
-    frame_actual_gray = cv2.cvtColor(frame_actual, cv2.COLOR_BGR2GRAY)
-    frame_number = 0
+def subplot4(img1, img2, img3, img4, share = False, t1 = "", t2 = "", t3 = "", t4=""):
+    """Recibe 4 imagenes y 4 titulos y las muestra en forma de subplot de 4x4"""
+    plt.figure()
+    if share:
+        ax = plt.subplot(141); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(142, sharex=ax, sharey=ax); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(143, sharex=ax, sharey=ax); imshow(img3, new_fig=False, title=t3)
+        plt.subplot(144, sharex=ax, sharey=ax); imshow(img4, new_fig=False, title=t4)
+        plt.show(block=True)
+    else:
+        plt.subplot(141); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(142); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(143); imshow(img3, new_fig=False, title=t3)
+        plt.subplot(144); imshow(img4, new_fig=False, title=t4)
+        plt.show(block=True)
 
-    while cap.isOpened():
-        ret, frame_siguiente = cap.read()
-        if not ret:
+
+def carga_frames(path):
+    """Funcion encargada de cargar todos los frames de un video dado"""
+    # Verifico la existencia del archivo
+    if path is None:
+        print("Error: Archivo inexistente")
+        return -1
+    # Capturo el video del path
+    cap = cv2.VideoCapture(path)  
+    # Almaceno en una lista los frames que tengan dados en la mesa
+    frames = []
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            # Pasamos a RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frames.append(frame)
+        else:  
             break
-        
-        frame_siguiente = cv2.resize(frame_siguiente, (frame_actual.shape[1], frame_actual.shape[0]))
-        frame_siguiente_gray = cv2.cvtColor(frame_siguiente, cv2.COLOR_BGR2GRAY)
+    return frames
 
-        diferencia = cv2.absdiff(frame_actual_gray, frame_siguiente_gray)
-        _, diferencia_bin = cv2.threshold(diferencia, 30, 255, cv2.THRESH_BINARY)
+# Segmentación y enmascaramiento
+def mask_y_mask_inv_segHSV(img, h_min = 65, h_max = 85, s_min = 100, s_max = 255):
+    """Recibe una imagen de una mesa de dados y calcula la mascara binaria de aplicar
+    segmentacion de color verde y la mascara inversa de la misma"""
+    # Define a range for segmentation based on H and S values
+    lims_inf = np.array([h_min, s_min, 50])
+    lims_sup = np.array([h_max, s_max, 255])
+    # Convert the image to HSV and create a mask
+    hsv_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    # Creo la mascara y normalizo a imagen binaria con 0 y 1
+    mask = np.uint8(cv2.inRange(hsv_image, lims_inf, lims_sup) / 255)
+    mask_inv = 1 - mask
+    return mask, mask_inv
 
-        num_pixeles_movimiento = np.sum(diferencia_bin > 0)
-        if num_pixeles_movimiento < umbral_movimiento and frame_number > 50:
-            cap.release()
-            return frame_actual
-        
-        frame_actual_gray = frame_siguiente_gray
-        frame_number += 1
+# Filtrado de componentes conectadas
+def filtrar_area(img_bin, th1 = 4500, th2 = 5500):
+    """Recibe una imagen binaria, calcula las componentes conectadas y filtra las
+    mismas por area entre [th1,th2]"""
+    connectivity = 8
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img_bin, connectivity, cv2.CV_32S)
+    for i in range(num_labels):
+        area = stats[i, cv2.CC_STAT_AREA]
+        if area < th1 or area > th2:
+            img_bin[labels==i] = 0
+            num_labels -= 1
+    return num_labels
 
-    cap.release()
-    raise ValueError("No se detectó el cese de movimiento en el video.")
+def filtrar_relAsp(img_bin, th1 = 0.8, th2 = 1.2):
+    """Filtra componentes conectadas por relación de aspecto."""
+    connectivity = 8
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img_bin, connectivity, cv2.CV_32S)
+    for i in range(num_labels):
+        alto = stats[i, cv2.CC_STAT_HEIGHT]
+        ancho = stats[i, cv2.CC_STAT_WIDTH]
+        relAsp = alto / ancho
+        if relAsp < th1 or relAsp > th2:
+            img_bin[labels==i] = 0
+            num_labels -= 1
+    return num_labels
 
-def extraer_area_interes(frame):
-    """
-    Detecta el área de interés en un frame y, si los dados están quietos, identifica las caras visibles.
-    """
-    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    fondo = frame[30:200, 30:200]
-    fondo_hsv = cv2.cvtColor(fondo, cv2.COLOR_BGR2HSV)
+def dados_en_mesa(img):
+    """Determina si la imagen tiene dados en la mesa. Con
+    las siguientes condiciones:
+        #Cantidad de pixels verdes mayor a umbral adecuado
+        #Area de los dados entre umbral adecuado"""
+    if img is None:
+        print("Error: debe pasar una imagen")
+        return -1
+    # imshow(img) # Mostrar paso a paso
+    mask, mask_inv = mask_y_mask_inv_segHSV(img, 65, 85, 100, 255)
+    # imshow(mask) # Mostrar paso a paso
+    cant_verde = mask.sum()
+    # imshow(mask_inv) # Mostrar paso a paso
+    # Calculamos componentes conectadas y filtramos por area para quedarnos solo con dados
+    filtrar_area(mask_inv, 4400, 6000) # Trabaja por referencia
+    # imshow(mask_inv) # Mostrar paso a paso
+    # Calculamos nuevamente las componentes conectadas y verificamos tener mas de 1 y menos de 6
+    # es decir, mas de 1 dado (por si algunos estan muy juntos o por si queda un dedo de la mano en la imagen)
+    # y menor o igual a 5 (ya que son 5 dados)
+    connectivity = 8
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask_inv, connectivity, cv2.CV_32S)
+    cant_dados = num_labels - 1
+    min_dados = 1
+    max_dados = 6 #Esto es por si hay un dedo en la imagen con el area de un dado lo tome igual
+    CANT_VERDE_TH = 1600000 # Se puede ajustar un poco mas bajo para obtener algunos frames con partes de manos al agarrar los dados, por ejemplos 1650000. Default 1.7 mill
+    cumple = cant_dados > min_dados and cant_dados <= max_dados and cant_verde > CANT_VERDE_TH
+    # Si esto no basta, podria obtener el umbral de cantidad de pixels verdes con dados arriba
+    # y agregarlo a la filtracion o condicion
+    return cumple
 
-    colores, counts = np.unique(fondo_hsv.reshape(-1, 3), axis=0, return_counts=True)
-    color_fondo = colores[np.argmax(counts)]
+# Filtrado de frames útiles
+def filtrar_frames_dados_estaticos(frames):
+    """Funcion encargada de filtrar los frames utiles de un video de tirada particular
+    Se filtran solo los frames que cumplen con la funcion predicado "dados_en_mesa()"
+    Luego se queda solo con la mitad superior de los frames para quedarse con los
+    frames que tienen dados estaticos en la mesa."""
+    # Almaceno en una lista los frames que tengan dados en la mesa
+    frames_dados = []
+    i_frame = 0
+    cant_frames_dados = 0
+    inicio = 0
+    for frame in frames:
+        if dados_en_mesa(frame):
+            if cant_frames_dados == 0:
+                inicio = i_frame
+            frames_dados.append(frame)
+            cant_frames_dados += 1
+        i_frame += 1
+    # Retorno la mitad superior -10 de la lista de frames con dados ya que es donde estan estaticos
+    largo = cant_frames_dados
+    k = 0
+    indice_inf = inicio + largo//2 - k
+    indice_sup = inicio + cant_frames_dados - k
+    frames_dados_estaticos = frames_dados[largo//2 - k:]
+    return frames_dados_estaticos,indice_inf, indice_sup
 
-    delta_bg = np.array([10, 150, 150])
-    lower_lim = np.clip(color_fondo - delta_bg, 0, 255)
-    upper_lim = np.clip(color_fondo + delta_bg, 0, 255)
 
-    mask = cv2.inRange(frame_hsv, lower_lim, upper_lim)
-    return mask
-
-def detectar_dados(mask, frame):
-    # Aplicar limpieza morfológica
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-    # Detectar componentes conectadas
-    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask)
-
-    dados = []
-    for i in range(1, num_labels):  # Saltar el fondo
-        x, y, w, h, area = stats[i]
-        if area > 600:  # Filtrar por tamaño mínimo
-            dado = frame[y:y+h, x:x+w]
-            dados.append(dado)
+def detecta_dados(frames_dados_estaticos):
+    """Recibe los frames con dados estaticos en la mesa, detecta los dados y los
+    identifica con un bounding box. Para esto toma un frame de referencia, en especifico
+    el frame del medio de la lista, y arma los bounding box a partir del mismo para
+    todas los demas frames con dados estaticos ya que estaran en el mismo lugar aprox.
+    Devuelve todos los dados identificados y la mascara de los bounding box"""
+    dados_identificados = [frame.copy() for frame in frames_dados_estaticos]
+    indice_ref = len(frames_dados_estaticos) // 2
+    frame_ref = frames_dados_estaticos[indice_ref]
+    frame_ref_c = frame_ref.copy()
+    frame_ref_gray = cv2.cvtColor(frame_ref_c, cv2.COLOR_RGB2GRAY)
+    mask_box = np.zeros_like(frame_ref_gray)
+    mask, mask_inv = mask_y_mask_inv_segHSV(frame_ref_c, 60, 85, 100, 255) 
+    if filtrar_area(mask_inv, 4400, 6200) != 5: #Si todavia no se consiguieron los 5 dados filtro por relacion de aspecto
+        filtrar_relAsp(mask_inv, 0.8, 1.2)
+    subplot3(frame_ref_c, mask, mask_inv, True) # Mostrar paso a paso !
+    connectivity = 8
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask_inv, connectivity, cv2.CV_32S)
+    cant_dados = num_labels - 1
+    for st in stats[1:]:
+            x1, y1, x2, y2 = st[0], st[1], st[0]+st[2],  st[1]+st[3]
+            dado = frame_ref_c[y1:y2, x1:x2].copy()
+            dado_gray = cv2.cvtColor(dado, cv2.COLOR_RGB2GRAY)
+            dado_gray_blur = cv2.medianBlur(dado_gray,7)
+            _,mask = cv2.threshold(dado_gray_blur, 170, 255,cv2.THRESH_BINARY)
+            # subplot4(dado, dado_gray, dado_gray_blur, mask, True) # Mostrar paso a paso !
+            connectivity = 8
+            num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity, cv2.CV_32S) #Detectamos circulos con este metodo porque no funciono houghcircles probando con diferentes parametros, ademas es mas eficiente
+            numero_dado = num_labels - 1
+            cv2.rectangle(mask_box, (x1, y1), (x2, y2), 255, thickness=3)
+            k1 = 19
+            k2 = 5
+            pos = (x1 + (x2-x1)//2 - k1, y1-k2)
+            text = str(numero_dado)
+            cv2.putText(mask_box, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 2, 255, 3)
+    frame_ref_c[mask_box == 255] = [0,0,255]
+    subplot3(frame_ref,mask_box,frame_ref_c, True)
+    imshow(frame_ref_c)
+    # Bounding box a todos los frames
+    for frame in dados_identificados:
+        frame[mask_box == 255] = [0,0,255]
+    return dados_identificados, mask_box
     
-    return dados
 
-def contar_puntos(dado):
-    dado_gray = cv2.cvtColor(dado, cv2.COLOR_BGR2GRAY)
-    dado_blur = cv2.GaussianBlur(dado_gray, (5, 5), 0)
-    circles = cv2.HoughCircles(dado_blur, cv2.HOUGH_GRADIENT, dp=1.2, minDist=20, param1=50, param2=15, minRadius=5, maxRadius=15)
-    return len(circles[0]) if circles is not None else 0
+def grabar_video(frames_out, tirada):
+    """Recibe frames con dados estaticos en la mesa y detecta los dados
+    Para la deteccion se usa el frame del medio
+    Para la identificacion se usan todos los frames"""
+    cap = cv2.VideoCapture(r'inputs\tirada_' + str(tirada) + '.mp4')  
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  
+    fps = int(cap.get(cv2.CAP_PROP_FPS))  
+    out = cv2.VideoWriter(r'outputs\output_tirada_' + str(tirada) +'.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
+    for frame in frames_out:
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame_show = cv2.resize(frame, dsize=(int(width/3), int(height/3)))
+        # frame_show = cv2.cvtColor(frame_show, cv2.COLOR_RGB2BGR)
+        cv2.imshow("frame",frame_show)
+        if cv2.waitKey(25) & 0xFF == ord('q'): 
+            cv2.destroyAllWindows()
+        out.write(frame)   
+    cap.release() 
+    out.release() 
+    cv2.destroyAllWindows() 
 
-# Procesar cada video
-for i in range(1, 5):
-    video_path = r'videos\tirada_' + str(i) + '.mp4'
-    print("Procesando: tirada_" + str(i))
-    frame_final = detectar_movimiento(video_path)
-    imshow(frame_final, title="Frame")
-    mask = extraer_area_interes(frame_final)
-    imshow(mask, title="Mascara")
-    dados = detectar_dados(mask, frame_final)
 
-    for idx, dado in enumerate(dados):
-        puntos = contar_puntos(dado)
-        print(f"Dado {idx+1}: {puntos} puntos.")
+
+tirada = 3
+# 1 # Se cargan todos los frames en una lista
+frames = carga_frames(r'inputs\tirada_' + str(tirada) + '.mp4')
+# len(frames)
+copy_frames = [frame.copy() for frame in frames] # Se crea una copia para no modificar las imagenes originales con bounding box
+
+# 2 # Se obtienen los frames con dados y los indices con respecto la lista frames.
+    # Estos indices nos serviran luego para grabar el video final, modificando los 
+    # frames que solo esten dentro de esos indices y dejando el resto iguales
+frames_dados_estaticos, indice_inf, indice_sup = filtrar_frames_dados_estaticos(copy_frames)
+# Vemos los primeros frames hasta el ultimo con dados estaticos
+# subplot4(frames_dados_estaticos[0], frames_dados_estaticos[1], frames_dados_estaticos[-2], frames_dados_estaticos[-1])
+
+# 3 # Identificamos los dados con bounding box en todos los frames para poder grabar el video
+dados_identificados, mask_box = detecta_dados(frames_dados_estaticos)
+# subplot4(dados_identificados[0], dados_identificados[1], dados_identificados[-2], dados_identificados[-1])
+
+
+# 4 # Agregamos los frames con los bounding box a todos los frames en su posicion correspondiente
+copy_frames[indice_inf:indice_sup] = dados_identificados
+frames_out = copy_frames
+
+# 5 # Grabamos el video con los frames resultantes
+grabar_video(frames_out, tirada)
---
+++ segmentacionHSV.py
@@ -0,0 +1,258 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+
+"""                            Descripción:
+Este script fue hecho para calcular de forma dinamica los umbrales de valores HSV.
+Mediante una GUI interactiva de OpenCV se obtiene una segmentacion mas visual y 
+eficiente a la vez.
+"""
+
+def imshow(img, new_fig=True, title=None, color_img=False, blocking=False, colorbar=False, ticks=False):
+    """
+    Definimos funcion para simplificar codigo de visualizaciones
+    """
+    if new_fig:
+        plt.figure()
+    if color_img:
+        plt.imshow(img)
+    else:
+        plt.imshow(img, cmap='gray')
+    plt.title(title)
+    if not ticks:
+        plt.xticks([]), plt.yticks([])
+    if colorbar:
+        plt.colorbar()
+    if new_fig:        
+        plt.show(block=blocking)
+
+def subplot4(img1, img2, img3, img4, share = False, t1 = "", t2 = "", t3 = "", t4=""):
+    """
+    Recibe 4 imagenes y 4 titulos y las muestra en forma de subplot de 4x4
+    """
+    plt.figure()
+    if share:
+        ax = plt.subplot(141); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(142, sharex=ax, sharey=ax); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(143, sharex=ax, sharey=ax); imshow(img3, new_fig=False, title=t3)
+        plt.subplot(144, sharex=ax, sharey=ax); imshow(img4, new_fig=False, title=t4)
+        plt.show(block=False)
+    else:
+        plt.subplot(141); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(142); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(143); imshow(img3, new_fig=False, title=t3)
+        plt.subplot(144); imshow(img4, new_fig=False, title=t4)
+        plt.show(block=False)
+
+def subplot3(img1, img2, img3, share = False, t1 = "", t2 = "", t3 = ""):
+    """
+    Recibe 3 imagenes y 3 titulos y las muestra en forma de subplot de 3x3
+    """
+    plt.figure()
+    if share:
+        ax = plt.subplot(131); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(132, sharex=ax, sharey=ax); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(133, sharex=ax, sharey=ax); imshow(img3, new_fig=False, title=t3)
+        plt.show(block=False)
+    else: 
+        plt.subplot(131); imshow(img1, new_fig=False, title=t1)
+        plt.subplot(132); imshow(img2, new_fig=False, title=t2)
+        plt.subplot(133); imshow(img3, new_fig=False, title=t3)
+        plt.show(block=False)
+
+
+def guarda_frames(n_tirada,detalles = False, view = False):
+    """
+    Recibe el numero de tirada de un video y guarda todos los frames
+    del mismo en la carpeta frames.
+    Además, se puede especificar si se desea ver los detalles del video y 
+    el video en cuestion
+    """
+    # Creamos carpeta frames si no existe
+    os.makedirs("frames", exist_ok = True)  
+    # Capturamos el video en cuestion y calculamos
+    cap = cv2.VideoCapture("tirada_"+ str(n_tirada) + ".mp4")
+    # Si se requiere mostramos detalles del video
+    if detalles:
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        print("-" * 20)
+        print("Detalles del video")
+        print(" - Ancho:", width, "px")
+        print(" - Alto:", height, "px")
+        print(" - FPS:", fps)
+        print("-" * 20)
+    # Guardamos los frames en la carpeta correspondiente y mostramos los mismos
+    frame_number = 0
+    while (cap.isOpened()): 
+        ret, frame = cap.read() 
+        if ret == True:
+            frame = cv2.resize(frame, dsize=(int(width/3), int(height/3)))
+            if view:
+                cv2.imshow('Frame', frame) 
+            cv2.imwrite(os.path.join("frames", f"frame_{frame_number}.jpg"), frame) 
+            frame_number += 1
+            if cv2.waitKey(25) & 0xFF == ord('q'): 
+                break
+        else:  
+            break
+    cv2.destroyAllWindows()
+    cap.release()
+    print(frame_number, "Frames guardados exitosamente")
+
+
+def mask_y_mask_inv_segHSV(img, h1_min = 65, h1_max = 85, s1_min = 100, s1_max = 255, h2_min = None, h2_max = None, s2_min = None, s2_max = None):
+    """
+    Recibe una imagen en formato RGB y calcula la mascara binaria de aplicar segmentacion de
+    color especificado en formato HSV. Ademas devuelve la mascara inversa de la misma.
+    Por default calcula la mascara binaria de segmentar por color verde
+    """
+    # Definimos los limites infereriores y superirores
+    lims_inf = np.array([h1_min, s1_min, 50])
+    lims_sup = np.array([h1_max, s1_max, 255])
+    # Convertimos la imagen a formato HSV
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    if h2_min: # Si hay 2 intervalos para cada valor HSV (por ejemplo para segmentar el color rojo)
+        lims2_inf = np.array([h2_min, s2_min, 50])
+        lims2_sup = np.array([h2_max, s2_max, 255])
+        # Calculamos las mascaras binarias con los limites dados
+        mask1 = cv2.inRange(hsv_img, lims_inf, lims_sup)
+        mask2 = cv2.inRange(hsv_img, lims2_inf, lims2_sup)
+        # Combinamos ambas máscaras
+        mask = cv2.bitwise_or(mask1, mask2)
+    else: # Si hay un solo intervalo para cada valor HSV
+        mask = cv2.inRange(hsv_img, lims_inf, lims_sup)
+    # Normalizo la mascara a 0s y 1s y calculo la mascara inversa 
+    mask = np.uint8(mask / 255) 
+    mask_inv = 1 - mask
+    return mask, mask_inv
+
+def update_image(img):
+    """
+    Funcion para segmentar imagen por color mediante valores de H, S y V del formato HSV
+    """
+    titulo = "Segmentacion dinamica HSV"
+    h_min = cv2.getTrackbarPos("Hue Min", titulo)
+    h_max = cv2.getTrackbarPos("Hue Max", titulo)
+    s_min = cv2.getTrackbarPos("Saturation Min", titulo)
+    s_max = cv2.getTrackbarPos("Saturation Max", titulo)
+    v_min = cv2.getTrackbarPos("Value Min", titulo)
+    v_max = cv2.getTrackbarPos("Value Max", titulo)
+    # Define a range for segmentation based on H, S, and V values
+    lims_inf = np.array([h_min, s_min, v_min])
+    lims_sup = np.array([h_max, s_max, v_max])
+    # Convert the image to HSV and create a mask
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_img, lims_inf, lims_sup)
+    # Apply the mask to the original img
+    segmentacion = cv2.bitwise_and(img, img, mask=mask)
+    # Show the segmented result
+    cv2.imshow(titulo, segmentacion)
+
+def segmentacion_dinamica(img):
+    """
+    Recibe una imagen en formato RGB y crea la GUI interactiva para segmentar por colores
+    en formato HSV
+    """
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # Callback con la imagen específica
+    def callback(_):
+        update_image(img)
+    # Convertimos la imagen a hsv para aplicar la segmentacion de colores
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # Creamos la GUI interactiva
+    titulo = "Segmentacion dinamica HSV"
+    cv2.namedWindow(titulo)
+    cv2.createTrackbar("Hue Min", titulo, 0, 179, callback)
+    cv2.createTrackbar("Hue Max", titulo, 179, 179, callback)
+    cv2.createTrackbar("Saturation Min", titulo, 0, 255, callback)
+    cv2.createTrackbar("Saturation Max", titulo, 255, 255, callback)
+    cv2.createTrackbar("Value Min", titulo, 0, 255, callback)
+    cv2.createTrackbar("Value Max", titulo, 255, 255, callback)
+    # Calculamos valores minimos y maximos para cada valor HSV de la imagen
+    h_min = int(np.min(hsv_img[:, :, 0]))
+    h_max = int(np.max(hsv_img[:, :, 0]))
+    s_min = int(np.min(hsv_img[:, :, 1]))
+    s_max = int(np.max(hsv_img[:, :, 1]))
+    v_min = int(np.min(hsv_img[:, :, 2]))
+    v_max = int(np.max(hsv_img[:, :, 2]))
+    # Creamos los trackbars 
+    cv2.setTrackbarPos("Hue Min", titulo, h_min)
+    cv2.setTrackbarPos("Hue Max", titulo, h_max)
+    cv2.setTrackbarPos("Saturation Min", titulo, s_min)
+    cv2.setTrackbarPos("Saturation Max", titulo, s_max)
+    cv2.setTrackbarPos("Value Min", titulo, v_min)
+    cv2.setTrackbarPos("Value Max", titulo, v_max)
+    # Mostramos la imagen original segmentada
+    cv2.imshow(titulo, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+# Guardamos los frames de la tirada de dados deseada
+n_tirada = 1
+guarda_frames(n_tirada, True, True)
+
+# Cargamos el frame que deseamos segmentar
+n_frame = 70 # Probar con frame 0, 20, 70, 100
+path = "frames/frame_" + str(n_frame) +".jpg"  
+img = cv2.imread(path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Pasamos a formato RGB
+if img is None:
+    print("Error: imagen inexistente")
+    exit(1)
+imshow(img)
+
+# Segmentamos dinamicamente la image con la GUI interactiva
+segmentacion_dinamica(img)
+
+# A partir de la segmentacion dinamica anterior obtuvimos los siguientes umbrales:
+# Umbrales para color:
+
+# VERDE
+    # Hmin = 70
+    # Hmax = 85
+    # Smin = 100
+    # Smax = 255
+    # Vmin = 50
+    # Vmax = 255
+# Con la funcion queda:
+mask, mask_inv = mask_y_mask_inv_segHSV(img) # O especificando valores mask_y_mask_inv_segHSV(img, 65, 85, 100, 255)
+seg = cv2.bitwise_and(img, img, mask=mask)
+# Visualizamos el resultado
+subplot4(img,mask, mask_inv, seg,True, "Imagen original", "Mascara binaria color verde","Mascara binaria inversa color verde", "Segmentación")
+
+# ROJO
+    # H1min = 0
+    # H1max = 15
+    # S1min = 150
+    # S1max = 255
+    # H2min = 165
+    # H2max = 179
+    # S1min = 25
+    # S1max = 255
+    # Vmin  = 50
+    # Vmax  = 255
+
+# Con la funcion queda:
+mask, mask_inv = mask_y_mask_inv_segHSV(img, 0, 15, 150, 255, 165, 179, 25, 255)
+seg = cv2.bitwise_and(img, img, mask=mask)
+# Visualizamos el resultado
+subplot4(img,mask, mask_inv, seg,True, "Imagen original", "Mascara binaria color rojo","Mascara binaria inversa color rojo", "Segmentación")
+
+
+# MEJORAMIENTO DE MASCARA BINARIA DADOS. SACAR DE AQUI Y AGREGAR EN EL TP
+mask, mask_inv = mask_y_mask_inv_segHSV(img, 0, 15, 150, 255, 165, 179, 25, 255)
+subplot3(img,mask,mask_inv,True)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)) 
+dil = cv2.dilate(mask, kernel)
+seg = cv2.bitwise_and(img, img, mask=dil)
+subplot4(img,mask,dil,seg,True)
+
+
+connectivity = 8
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(dil, connectivity, cv2.CV_32S)
+cant_dados = num_labels - 1
